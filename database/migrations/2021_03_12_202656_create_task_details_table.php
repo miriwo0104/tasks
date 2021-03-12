691096291e289ee554243c9f45374821e217b94c
Shun Okawa <shunokawa@gmail.com>
2021-03-12 20:44:07 +0900

    task_detailsテーブルの作成
    モデルファイルとマイグレーションファイルの追加

---
+++ database/migrations/2021_03_12_202656_create_task_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('task_id')->comment('ユーザーid');
+            $table->text('detail')->comment('タスク詳細');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_details');
+    }
+}
