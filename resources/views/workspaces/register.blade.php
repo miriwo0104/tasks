0881eacaef82f77b3225738667285ba4ded15c0b
Shun Okawa <shunokawa@gmail.com>
2021-03-24 19:20:04 +0900

    ワークスペース登録画面のデザインを変更した

--- resources/views/workspaces/register.blade.php
+++ resources/views/workspaces/register.blade.php
@@ -9,20 +9,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <form action="{{ route('workspace.save') }}" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">ワークスペース名</label>
+                            @error('workspace_name')
+                                {{$message}}
+                            @enderror
+                            <input type="text" name="workspace_name" value="{{ old('workspace_name') }}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <button type="submit" class="btn btn-primary">登録</button>
+                        </div>
+                    </form>
                 </div>
             </div>
-            <form action="{{ route('workspace.save') }}" method="post">
-                @csrf
-                <div>
-                    ワークスペース名を入力
-                </div>
-                @error('workspace_name')
-                    {{$message}}
-                @enderror
-                <input type="text" name="workspace_name" value="{{ old('workspace_name') }}">
-                <input type="submit" value="保存">
-            </form>
         </div>
     </div>
 </x-app-layout>
