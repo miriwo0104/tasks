2108e870fef4290636c182be2623ccc326e935e1
Shun Okawa <shunokawa@gmail.com>
2021-03-24 21:02:08 +0900

    ワークスペース詳細画面のデザインを修正した

--- resources/views/workspaces/detail.blade.php
+++ resources/views/workspaces/detail.blade.php
@@ -23,17 +23,20 @@
                         @if (!is_null($post_data['task_infos']['today_task_infos']))
                             <ol>
                                 @foreach ($post_data['task_infos']['today_task_infos'] as $today_task_info)
-                                    <li type="disc">{{ $today_task_info['name'] }}
-                                        <form action="{{ route('task.complete') }}" method="post">
-                                            @csrf
-                                            <input type="hidden" name="workspace_id" value="{{ $post_data['workspace_info']['id'] }}">
-                                            <input type="hidden" name="task_id" value="{{ $today_task_info['id'] }}">
-                                            <button type="submit" type="button" class="btn btn-success">完了にする</button>
-                                        </form>
-                                        <a href="{{ route('task.detail', ['workspace_id' => $post_data['workspace_info']['id'], 'task_id' => $today_task_info['id']]) }}">
-                                            <button type="button" class="btn btn-secondary">詳細</button>
-                                        </a>
-                                    </li>
+                                    <div class="card" style="width: 100%">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{ $today_task_info['name'] }}</h5>
+                                            <form action="{{ route('task.complete') }}" method="post">
+                                                @csrf
+                                                <input type="hidden" name="workspace_id" value="{{ $post_data['workspace_info']['id'] }}">
+                                                <input type="hidden" name="task_id" value="{{ $today_task_info['id'] }}">
+                                                <button type="submit" type="button" class="btn btn-success card-link">完了にする</button>
+                                            </form>
+                                            <a href="{{ route('task.detail', ['workspace_id' => $post_data['workspace_info']['id'], 'task_id' => $today_task_info['id']]) }}">
+                                                <button type="button" class="btn btn-secondary card-link">詳細</button>
+                                            </a>
+                                        </div>
+                                    </div>
                                 @endforeach
                             </ol>
                         @else
@@ -47,17 +50,20 @@
                         @if (!is_null($post_data['task_infos']['tomorrow_task_infos']))
                             <ol>
                                 @foreach ($post_data['task_infos']['tomorrow_task_infos'] as $tomorrow_task_info)
-                                    <li type="disc">{{ $tomorrow_task_info['name'] }}
-                                        <form action="{{ route('task.complete') }}" method="post">
-                                            @csrf
-                                            <input type="hidden" name="workspace_id" value="{{ $post_data['workspace_info']['id'] }}">
-                                            <input type="hidden" name="task_id" value="{{ $tomorrow_task_info['id'] }}">
-                                            <button type="submit" type="button" class="btn btn-success">完了にする</button>
-                                        </form>
-                                        <a href="{{ route('task.detail', ['workspace_id' => $post_data['workspace_info']['id'], 'task_id' => $tomorrow_task_info['id']]) }}">
-                                            <button type="button" class="btn btn-secondary">詳細</button>
-                                        </a>
-                                    </li>
+                                    <div class="card" style="width: 100%">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{ $tomorrow_task_info['name'] }}</h5>
+                                            <form action="{{ route('task.complete') }}" method="post">
+                                                @csrf
+                                                <input type="hidden" name="workspace_id" value="{{ $post_data['workspace_info']['id'] }}">
+                                                <input type="hidden" name="task_id" value="{{ $tomorrow_task_info['id'] }}">
+                                                <button type="submit" type="button" class="btn btn-success card-link">完了にする</button>
+                                            </form>
+                                            <a href="{{ route('task.detail', ['workspace_id' => $post_data['workspace_info']['id'], 'task_id' => $tomorrow_task_info['id']]) }}">
+                                                <button type="button" class="btn btn-secondary card-link">詳細</button>
+                                            </a>
+                                        </div>
+                                    </div>
                                 @endforeach
                             </ol>
                         @else
@@ -72,17 +78,20 @@
                         @if (!is_null($post_data['task_infos']['week_task_infos']))
                             <ol>
                                 @foreach ($post_data['task_infos']['week_task_infos'] as $week_task_info)
-                                    <li type="disc">{{ $week_task_info['name'] }}
-                                        <form action="{{ route('task.complete') }}" method="post">
-                                            @csrf
-                                            <input type="hidden" name="workspace_id" value="{{ $post_data['workspace_info']['id'] }}">
-                                            <input type="hidden" name="task_id" value="{{ $week_task_info['id'] }}">
-                                            <button type="submit" type="button" class="btn btn-success">完了にする</button>
-                                        </form>
-                                        <a href="{{ route('task.detail', ['workspace_id' => $post_data['workspace_info']['id'], 'task_id' => $week_task_info['id']]) }}">
-                                            <button type="button" class="btn btn-secondary">詳細</button>
-                                        </a>
-                                    </li>
+                                    <div class="card" style="width: 100%">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{ $week_task_info['name'] }}</h5>
+                                            <form action="{{ route('task.complete') }}" method="post">
+                                                @csrf
+                                                <input type="hidden" name="workspace_id" value="{{ $post_data['workspace_info']['id'] }}">
+                                                <input type="hidden" name="task_id" value="{{ $week_task_info['id'] }}">
+                                                <button type="submit" type="button" class="btn btn-success card-link">完了にする</button>
+                                            </form>
+                                            <a href="{{ route('task.detail', ['workspace_id' => $post_data['workspace_info']['id'], 'task_id' => $week_task_info['id']]) }}">
+                                                <button type="button" class="btn btn-secondary card-link">詳細</button>
+                                            </a>
+                                        </div>
+                                    </div>
                                 @endforeach
                             </ol>
                         @else
@@ -96,17 +105,20 @@
                         @if (!is_null($post_data['task_infos']['month_task_infos']))
                             <ol>
                                 @foreach ($post_data['task_infos']['month_task_infos'] as $month_task_info)
-                                    <li type="disc">{{ $month_task_info['name'] }}
-                                        <form action="{{ route('task.complete') }}" method="post">
-                                            @csrf
-                                            <input type="hidden" name="workspace_id" value="{{ $post_data['workspace_info']['id'] }}">
-                                            <input type="hidden" name="task_id" value="{{ $month_task_info['id'] }}">
-                                            <button type="submit" type="button" class="btn btn-success">完了にする</button>
-                                        </form>
-                                        <a href="{{ route('task.detail', ['workspace_id' => $post_data['workspace_info']['id'], 'task_id' => $month_task_info['id']]) }}">
-                                            <button type="button" class="btn btn-secondary">詳細</button>
-                                        </a>
-                                    </li>
+                                    <div class="card" style="width: 100%">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{ $month_task_info['name'] }}</h5>
+                                            <form action="{{ route('task.complete') }}" method="post">
+                                                @csrf
+                                                <input type="hidden" name="workspace_id" value="{{ $post_data['workspace_info']['id'] }}">
+                                                <input type="hidden" name="task_id" value="{{ $month_task_info['id'] }}">
+                                                <button type="submit" type="button" class="btn btn-success card-link">完了にする</button>
+                                            </form>
+                                            <a href="{{ route('task.detail', ['workspace_id' => $post_data['workspace_info']['id'], 'task_id' => $month_task_info['id']]) }}">
+                                                <button type="button" class="btn btn-secondary card-link">詳細</button>
+                                            </a>
+                                        </div>
+                                    </div>
                                 @endforeach
                             </ol>
                         @else
@@ -120,17 +132,20 @@
                         @if (!is_null($post_data['task_infos']['undecided_task_infos']))
                             <ol>
                                 @foreach ($post_data['task_infos']['undecided_task_infos'] as $undecide_task_info)
-                                    <li type="disc">{{ $undecide_task_info['name'] }}
-                                        <form action="{{ route('task.complete') }}" method="post">
-                                            @csrf
-                                            <input type="hidden" name="workspace_id" value="{{ $post_data['workspace_info']['id'] }}">
-                                            <input type="hidden" name="task_id" value="{{ $undecide_task_info['id'] }}">
-                                            <button type="submit" type="button" class="btn btn-success">完了にする</button>
-                                        </form>
-                                        <a href="{{ route('task.detail', ['workspace_id' => $post_data['workspace_info']['id'], 'task_id' => $undecide_task_info['id']]) }}">
-                                            <button type="button" class="btn btn-secondary">詳細</button>
-                                        </a>
-                                    </li>
+                                    <div class="card" style="width: 100%">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{ $undecide_task_info['name'] }}</h5>
+                                            <form action="{{ route('task.complete') }}" method="post">
+                                                @csrf
+                                                <input type="hidden" name="workspace_id" value="{{ $post_data['workspace_info']['id'] }}">
+                                                <input type="hidden" name="task_id" value="{{ $undecide_task_info['id'] }}">
+                                                <button type="submit" type="button" class="btn btn-success card-link">完了にする</button>
+                                            </form>
+                                            <a href="{{ route('task.detail', ['workspace_id' => $post_data['workspace_info']['id'], 'task_id' => $undecide_task_info['id']]) }}">
+                                                <button type="button" class="btn btn-secondary card-link">詳細</button>
+                                            </a>
+                                        </div>
+                                    </div>
                                 @endforeach
                             </ol>
                         @else
@@ -149,7 +164,7 @@
                                             @csrf
                                             <input type="hidden" name="workspace_id" value="{{ $post_data['workspace_info']['id'] }}">
                                             <input type="hidden" name="task_id" value="{{ $complete_task_info['id'] }}">
-                                            <button type="submit" type="button" class="btn btn-success">未完了にする</button>
+                                            <button type="submit" type="button" class="btn btn-warning">未完了にする</button>
                                         </form>
                                         <a href="{{ route('task.detail', ['workspace_id' => $post_data['workspace_info']['id'], 'task_id' => $complete_task_info['id']]) }}">
                                             <button type="button" class="btn btn-secondary">詳細</button>
