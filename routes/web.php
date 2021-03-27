ac40d3f71d4c3f393af49f6b3df937d79229aac3
Shun Okawa <shunokawa@gmail.com>
2021-03-27 12:40:38 +0900

    各ページにログイン必須のミドルウェアをかけた

--- routes/web.php
+++ routes/web.php
@@ -26,16 +26,16 @@ Route::get('/dashboard', function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/top', [TopController::class, 'index'])->name('top.index');
+Route::get('/top', [TopController::class, 'index'])->middleware(['auth'])->name('top.index');
 
-Route::get('/workspace/register', [WorkspaceController::class, 'register'])->name('workspace.register');
-Route::post('/workspace/save', [WorkspaceController::class, 'save'])->name('workspace.save');
-Route::get('/workspace/detail/{workspace_id}', [WorkspaceController::class, 'detail'])->name('workspace.detail');
+Route::get('/workspace/register', [WorkspaceController::class, 'register'])->middleware(['auth'])->name('workspace.register');
+Route::post('/workspace/save', [WorkspaceController::class, 'save'])->middleware(['auth'])->name('workspace.save');
+Route::get('/workspace/detail/{workspace_id}', [WorkspaceController::class, 'detail'])->middleware(['auth'])->name('workspace.detail');
 
-Route::get('/task/register/{workspace_id}', [TaskController::class, 'register'])->name('task.register');
-Route::post('/task/save', [TaskController::class, 'save'])->name('task.save');
-Route::post('/task/status/change/complete', [TaskController::class, 'changeComplete'])->name('task.complete');
-Route::post('/task/status/change/incomplete', [TaskController::class, 'changeIncomplete'])->name('task.incomplete');
-Route::get('/task/detail/{workspace_id}/{task_id}', [TaskController::class, 'detail'])->name('task.detail');
-Route::get('/task/edit/{workspace_id}/{task_id}', [TaskController::class, 'edit'])->name('task.edit');
-Route::post('/task/update', [TaskController::class, 'update'])->name('task.update');
+Route::get('/task/register/{workspace_id}', [TaskController::class, 'register'])->middleware(['auth'])->name('task.register');
+Route::post('/task/save', [TaskController::class, 'save'])->middleware(['auth'])->name('task.save');
+Route::post('/task/status/change/complete', [TaskController::class, 'changeComplete'])->middleware(['auth'])->name('task.complete');
+Route::post('/task/status/change/incomplete', [TaskController::class, 'changeIncomplete'])->middleware(['auth'])->name('task.incomplete');
+Route::get('/task/detail/{workspace_id}/{task_id}', [TaskController::class, 'detail'])->middleware(['auth'])->name('task.detail');
+Route::get('/task/edit/{workspace_id}/{task_id}', [TaskController::class, 'edit'])->middleware(['auth'])->name('task.edit');
+Route::post('/task/update', [TaskController::class, 'update'])->middleware(['auth'])->name('task.update');
