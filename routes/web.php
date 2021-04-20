36bdae0af217c6f38caf6697fe872dd95849c87d
Shun Okawa <shunokawa@gmail.com>
2021-04-20 17:37:46 +0900

    ルーティングの冗長性を排除
    ミドルウェア

--- routes/web.php
+++ routes/web.php
@@ -29,34 +29,37 @@ require __DIR__.'/auth.php';
 
 Route::get('/top', [TopController::class, 'index'])->middleware(['auth'])->name('top.index');
 
-// ワークスペース系
-Route::prefix('workspace')->name('workspace.')->group(function () {
-    Route::get('/register', [WorkspaceController::class, 'register'])->middleware(['auth'])->name('register');
-    Route::post('/save', [WorkspaceController::class, 'save'])->middleware(['auth'])->name('save');
-    Route::get('/detail/task/{workspace_id}', [WorkspaceController::class, 'taskDetail'])->middleware(['auth'])->name('task.detail');
-    Route::get('/detail/memo/{workspace_id}', [WorkspaceController::class, 'memoDetail'])->middleware(['auth'])->name('memo.detail');
-});
+// ログイン必須ページ
+Route::middleware('auth')->group(function() {
+    // ワークスペース系
+    Route::prefix('workspace')->name('workspace.')->group(function () {
+        Route::get('/register', [WorkspaceController::class, 'register'])->name('register');
+        Route::post('/save', [WorkspaceController::class, 'save'])->name('save');
+        Route::get('/detail/task/{workspace_id}', [WorkspaceController::class, 'taskDetail'])->name('task.detail');
+        Route::get('/detail/memo/{workspace_id}', [WorkspaceController::class, 'memoDetail'])->name('memo.detail');
+    });
 
-// タスク系
-Route::prefix('task')->name('task.')->group(function () {
-    Route::get('/register/{workspace_id}', [TaskController::class, 'register'])->middleware(['auth'])->name('register');
-    Route::post('/save', [TaskController::class, 'save'])->middleware(['auth'])->name('save');
-    Route::post('/status/change/complete', [TaskController::class, 'changeComplete'])->middleware(['auth'])->name('complete');
-    Route::post('/status/change/incomplete', [TaskController::class, 'changeIncomplete'])->middleware(['auth'])->name('incomplete');
-    Route::get('/detail/{workspace_id}/{task_id}', [TaskController::class, 'detail'])->middleware(['auth'])->name('detail');
-    Route::get('/edit/{workspace_id}/{task_id}', [TaskController::class, 'edit'])->middleware(['auth'])->name('edit');
-    Route::post('/update', [TaskController::class, 'update'])->middleware(['auth'])->name('update');
-    Route::post('/delete', [TaskController::class, 'delete'])->middleware(['auth'])->name('delete');
-    Route::post('/revive', [TaskController::class, 'revive'])->middleware(['auth'])->name('revive');
-});
+    // タスク系
+    Route::prefix('task')->name('task.')->group(function () {
+        Route::get('/register/{workspace_id}', [TaskController::class, 'register'])->name('register');
+        Route::post('/save', [TaskController::class, 'save'])->name('save');
+        Route::post('/status/change/complete', [TaskController::class, 'changeComplete'])->name('complete');
+        Route::post('/status/change/incomplete', [TaskController::class, 'changeIncomplete'])->name('incomplete');
+        Route::get('/detail/{workspace_id}/{task_id}', [TaskController::class, 'detail'])->name('detail');
+        Route::get('/edit/{workspace_id}/{task_id}', [TaskController::class, 'edit'])->name('edit');
+        Route::post('/update', [TaskController::class, 'update'])->name('update');
+        Route::post('/delete', [TaskController::class, 'delete'])->name('delete');
+        Route::post('/revive', [TaskController::class, 'revive'])->name('revive');
+    });
 
-// メモ系
-Route::prefix('memo')->name('memo.')->group(function () {
-    Route::get('/register/{workspace_id}', [MemoController::class, 'register'])->middleware(['auth'])->name('register');
-    Route::post('/save', [MemoController::class, 'save'])->middleware(['auth'])->name('save');
-    Route::get('/detail/{workspace_id}/{memo_id}', [MemoController::class, 'detail'])->middleware(['auth'])->name('detail');
-    Route::get('/edit/{workspace_id}/{memo_id}', [MemoController::class, 'edit'])->middleware(['auth'])->name('edit');
-    Route::post('/update', [MemoController::class, 'update'])->middleware(['auth'])->name('update');
-    Route::post('/delete', [MemoController::class, 'delete'])->middleware(['auth'])->name('delete');
-    Route::post('/revive', [MemoController::class, 'revive'])->middleware(['auth'])->name('revive');
+    // メモ系
+    Route::prefix('memo')->name('memo.')->group(function () {
+        Route::get('/register/{workspace_id}', [MemoController::class, 'register'])->name('register');
+        Route::post('/save', [MemoController::class, 'save'])->name('save');
+        Route::get('/detail/{workspace_id}/{memo_id}', [MemoController::class, 'detail'])->name('detail');
+        Route::get('/edit/{workspace_id}/{memo_id}', [MemoController::class, 'edit'])->name('edit');
+        Route::post('/update', [MemoController::class, 'update'])->name('update');
+        Route::post('/delete', [MemoController::class, 'delete'])->name('delete');
+        Route::post('/revive', [MemoController::class, 'revive'])->name('revive');
+    });
 });
